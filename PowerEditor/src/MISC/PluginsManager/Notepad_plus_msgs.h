7816ae808020ef29a28aba621cd1642b51a9532b
Waldi Ravens <wr86420@gmail.com>
2019-07-03 07:11:05 +0200

    Add NPPM_GETBUFFERSTATUS (READONLY|MODIFIED|LOADEDDIRTY)
    PR ‡4242 by KlausiD

--- PowerEditor/src/MISC/PluginsManager/Notepad_plus_msgs.h
+++ PowerEditor/src/MISC/PluginsManager/Notepad_plus_msgs.h
@@ -1055,6 +1055,19 @@ enum Platform { PF_UNKNOWN, PF_X86, PF_X64, PF_IA64, PF_ARM64 };
 
 	#define	RUNCOMMAND_USER    (WM_USER + 3000)
 
+	#define BUFSTAT_READONLY     0x01
+	#define BUFSTAT_MODIFIED     0x02
+	#define BUFSTAT_LOADEDDIRTY  0x04
+	#define NPPM_GETBUFFERSTATUS (NPPMSG + 199)
+	// LRESULT NPPM_GETBUFFERSTATUS(UINT_PTR bufferID, 0)
+	// wParam: BufferID to get Status
+	// lParam: 0
+	// Return value is an LRESULT with bit flags
+	// BUFSTAT_READONLY    = 0x01 (Bit 0) set when Buffer is readonly
+	// BUFSTAT_MODIFIED    = 0x02 (Bit 1) set when Buffer is modified
+	// BUFSTAT_LOADEDDIRTY = 0x04 (Bit 2) set when Buffer is dirty
+	// or -1 when bufferId is invalid
+
 	#define NPPM_GETFULLCURRENTPATH		(RUNCOMMAND_USER + FULL_CURRENT_PATH)
 	#define NPPM_GETCURRENTDIRECTORY	(RUNCOMMAND_USER + CURRENT_DIRECTORY)
 	#define NPPM_GETFILENAME			(RUNCOMMAND_USER + FILE_NAME)
